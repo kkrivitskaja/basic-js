59bc38d2b8cd68b58707b8980469ae0f81c8aed9
kkrivitskaja <pratugames@gmail.com>
2020-10-28 16:42:13 +0300

    feat: implemet countCats function

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  if (!Array.isArray(matrix) || matrix.length == 0) return 0
+  let cat = matrix.reduce((a, b) => a.concat(b), []);
+  return cat.filter(elem => elem === '^^').length;
 };
+
+
