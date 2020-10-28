b815a37209a81e69f9243a347c9e7e26cb2a85ab
kkrivitskaja <pratugames@gmail.com>
2020-10-28 17:53:56 +0300

    feat: implement createDreamTeam function

--- src/count-cats.js
+++ src/count-cats.js
@@ -3,7 +3,7 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function countCats(matrix) {
   if (!Array.isArray(matrix) || matrix.length == 0) return 0
   let cat = matrix.reduce((a, b) => a.concat(b), []);
-  return cat.filter(elem => elem === '^^').length;
+    return cat.filter(elem => elem === '^^').length;
 };
 
 
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(membersTeam) {
+  if (!Array.isArray(membersTeam) || membersTeam.length == 0) return false
+  let dreamArr = [];
+  membersTeam.forEach(element => {
+    if (typeof element == typeof 'string') {
+      dreamArr.push(element.trim()[0].toUpperCase())
+    } 
+  });
+     return dreamArr.sort().join('');
+  
 };
+
