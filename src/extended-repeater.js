a53e3a61d7a7ca004fe4a75fbcb283856b77f897
kkrivitskaja <pratugames@gmail.com>
2020-11-01 02:48:13 +0300

    feat:implement string repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,21 +2,13 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
     str = String(str);
-
     let repeatTimes, separator, addition, additionRepeatTimes, additionSeparator;
-
     options.repeatTimes === undefined ? repeatTimes = 1 : repeatTimes = options.repeatTimes;
-
-    separator = options.separator || '+';
-
     options.addition === undefined ? addition = '' : addition = String(options.addition);
-
+    separator = options.separator || '+';
     options.additionRepeatTimes === undefined ? additionRepeatTimes = 1 : additionRepeatTimes = options.additionRepeatTimes;
-
-  additionSeparator = options.additionSeparator || '|';
-   let additionStr = ([].concat(...Array(Number.parseInt(options["additionRepeatTimes"])).fill(options["addition"].toString())).join(options["additionSeparator"].toString()));
-
-    let array = [].concat(...Array(Number.parseInt(options["repeatTimes"])).fill(str.toString() + additionStr));
-
-    return array.join(options["separator"].toString());
+    additionSeparator = options.additionSeparator || '|';
+    let newStr = ([].concat(...Array(Number.parseInt(additionRepeatTimes)).fill(addition.toString())).join(additionSeparator.toString()));
+    let array = [].concat(...Array(Number.parseInt(repeatTimes)).fill(str.toString() + newStr));
+    return array.join(separator.toString());
 };
