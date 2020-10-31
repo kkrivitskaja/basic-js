a9639b7445dc868b45dd5ef2caef46e68b0736b6
kkrivitskaja <pratugames@gmail.com>
2020-10-31 21:41:13 +0300

    feat: implement transform arr function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,7 +4,7 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  if (typeof sampleActivity !== 'string' || Number.isNaN(parseFloat(sampleActivity))|| sampleActivity<=0|| sampleActivity >15) { return false }
+  if (typeof sampleActivity !== 'string' || Number.isNaN(parseFloat(sampleActivity))|| sampleActivity<=0|| sampleActivity >MODERN_ACTIVITY) { return false }
   else{
   let ageOfExampl = Math.log10(MODERN_ACTIVITY / parseFloat(sampleActivity)) / 0.693 * HALF_LIFE_PERIOD;
    return ageOfExampl }
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) { throw new Error(); } else {
+    let transformArray = [];
+
+    for (let i = 0; i < arr.length; i++) {
+
+      switch (arr[i]) {
+        case '--discard-next':
+          i++;
+          break;
+        case '--discard-prev':
+          if (transformArray.length !== 0 && arr[i - 2] !== '--discard-next') {
+            transformArray.pop();
+          }
+          break;
+        case '--double-next':
+          if (i < arr.length - 1) {
+            transformArray.push(arr[i + 1]);
+          }
+          break;
+        case '--double-prev':
+          if (i - 1 > -1 && arr[i - 2] !== '--discard-next') {
+            transformArray.push(arr[i - 1]);
+          }
+          break;
+        default:
+          transformArray.push(arr[i]);
+      }
+    }
+    return transformArray;
+  }
 };
