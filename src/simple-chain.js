2dcb4d0b7aa346ab7692b1be269fbf0db3f8fbc8
kkrivitskaja <pratugames@gmail.com>
2020-11-01 22:11:05 +0300

    feat: implement VigenereCipheringMachine

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,32 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-  arrChain: [],
-  getLength() {
-    return this.arrChain.length;
-  },
-  addLink(value) {
-    this.arrChain.push(value);
-    return this;
-  },
-  removeLink(position) {
-    if(!Number.isInteger(position) || position - 1 < 0 || position > this.arrChain.length) {
+    arrChain: [],
+    getLength() {
+        return this.arrChain.length;
+    },
+    addLink(value) {
+        this.arrChain.push(value);
+        return this;
+    },
+    removeLink(position) {
+        if (!Number.isInteger(position) || position - 1 < 0 || position > this.arrChain.length) {
             this.arrChain = []
-      throw new Error();
-    } else {
-      this.arrChain.splice(position - 1, 1);
-      return this;
-    }
-  },
-  reverseChain() {
-    this.arrChain.reverse();
-    return this;
-  },
-  finishChain() {
-    const resalt = '( ' + this.arrChain.map((item) => '' + item).join(' )~~( ') + ' )';
-    this.arrChain.length = 0;
-    return resalt;
-  },
+            throw new Error();
+        } else {
+            this.arrChain.splice(position - 1, 1);
+            return this;
+        }
+    },
+    reverseChain() {
+        this.arrChain.reverse();
+        return this;
+    },
+    finishChain() {
+        const resalt = '( ' + this.arrChain.map((item) => '' + item).join(' )~~( ') + ' )';
+        this.arrChain.length = 0;
+        return resalt;
+    },
 };
 
 module.exports = chainMaker;
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,72 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+   constructor(type = true) {
+        this.typeEncr= type;
+    }
+
+    encrypt(string, key) {
+        this.checkValues(string, key);
+        let message = "";
+        let keyEl = 0,
+            stringEl = 0;
+        string = string.toString().toUpperCase();
+        key = key.toString().toUpperCase().replace(/[^A-Z]/g, '');
+
+        while (stringEl < string.length) {
+            keyEl = 0;
+            while (keyEl < key.length && stringEl < string.length) {
+                if (string[stringEl] >= 'A' && string[stringEl] <= 'Z') {
+                    let tmp = (key.charCodeAt(keyEl) + string.charCodeAt(stringEl)) % 26
+                    message += String.fromCharCode(tmp + 65);
+                    keyEl++;
+                } else {
+                    message += string[stringEl];
+                }
+                stringEl++;
+            }
+        }
+        return this.formatOutput(message);
+    }
+    decrypt(string, key) {
+        this.checkValues(string, key);
+        let message = "";
+        let keyEl = 0,
+            stringEl = 0;
+        string = string.toString().toUpperCase();
+        key = key.toString().toUpperCase().replace(/[^A-Z]/g, '');
+
+        while (stringEl < string.length) {
+            keyEl = 0;
+            while (stringEl < string.length&&keyEl < key.length ) {
+                if (string[stringEl] >= 'A' && string[stringEl] <= 'Z') {
+                    let tmp = (-key.charCodeAt(keyEl) + string.charCodeAt(stringEl) + 26) % 26
+                    message += String.fromCharCode(tmp + 65);
+                    keyEl++;
+                } else {
+                    message += string[stringEl];
+                }
+                stringEl++;
+            }
+        }
+        return this.formatOutput(message);
+    }
+    checkValues(string, key) {
+        if (string === undefined || key === null || key === undefined || string === null) throw new Error();
+    }
+  formatOutput(message) {
+    if (this.typeEncr) {
+        return message 
+    } else {
+      return message.split('').reverse().join('')
+      }
+        
+    }
 }
+
+
+
+
+
 
 module.exports = VigenereCipheringMachine;
